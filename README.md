a9627acba005f2fda41487081f82157a98bc40b1
Ryan Hamilton <rkhamilton@users.noreply.github.com>
2021-10-05 20:47:13 -0400

    project stub
    Some project definition info added

---
+++ README.md
@@ -0,0 +1,101 @@
+# VQGAN-CLIP-GENERATOR Overview
+
+A package for running VQGAN+CLIP locally. This package was a complete refactor of the code provided by [NerdyRodent](https://github.com/nerdyrodent/), which started out as a Katherine Crowson VQGAN+CLIP derived Google colab notebook.
+
+In addition to refactoring NerdyRodent's code into a pythonic package to improve usability, this project adds unit tests, and adds improvements to the ability to restyle an existing video.
+
+Original notebook: [![Open In Colab][colab-badge]][colab-notebook]
+
+[colab-notebook]: <https://colab.research.google.com/drive/1ZAus_gn2RhTZWzOWUpPERNC0Q8OhZRTZ>
+[colab-badge]: <https://colab.research.google.com/assets/colab-badge.svg>
+
+[NerdyRodent VQGAN+CLIP repository](https://github.com/nerdyrodent/)
+
+Some example images:
+
+<img src="./samples/A child throwing the ducks into a wood chipper painting by Rembrandt initial.png" width="256px"></img>
+<img src="./samples/Pastoral landscape painting in the impressionist style initial.png" width="256px"></img>
+<img src="./samples/The_sadness_of_Colonel_Sanders_by_Thomas_Kinkade.png" width="256px"></img>
+
+Environment:
+
+* Tested on Windows 10 build 19043
+* GPU: Nvidia RTX 3080
+* Typical VRAM requirements:
+  * 24 GB for a 900x900 image
+  * 10 GB for a 512x512 image
+  * 8 GB for a 380x380 image
+
+## Set up
+
+This example uses [Anaconda](https://www.anaconda.com/products/individual#Downloads) to manage virtual Python environments.
+
+Create a new virtual Python environment for VQGAN-CLIP-GENERATOR:
+
+```sh
+conda create --name vqgan python=3.9
+conda activate vqgan
+```
+
+### Using an AMD graphics card
+
+Instructions courtesy of NerdyRodent. Note: This hasn't been tested yet.
+
+ROCm can be used for AMD graphics cards instead of CUDA. You can check if your card is supported here:
+<https://github.com/RadeonOpenCompute/ROCm#supported-gpus>
+
+Install ROCm accordng to the instructions and don't forget to add the user to the video group:
+<https://rocmdocs.amd.com/en/latest/Installation_Guide/Installation-Guide.html>
+
+The usage and set up instructions above are the same, except for the line where you install Pytorch.
+Instead of `pip install torch==1.9.0+cu111 ...`, use the one or two lines which are displayed here (select Pip -> Python-> ROCm):
+<https://pytorch.org/get-started/locally/>
+
+### Using the CPU
+
+If no graphics card can be found, the CPU is automatically used and a warning displayed.
+
+Regardless of an available graphics card, the CPU can also be used by adding this command line argument: `-cd cpu`
+
+This works with the CUDA version of Pytorch, even without CUDA drivers installed, but doesn't seem to work with ROCm as of now.
+
+### Uninstalling
+
+Remove the Python enviroment:
+
+```sh
+conda remove --name vqgan --all
+```
+
+## Run
+
+To generate images from text, specify your text prompt as shown in the example below:
+```sh
+# TODO 
+```
+
+## Citations
+
+```bibtex
+@misc{unpublished2021clip,
+    title  = {CLIP: Connecting Text and Images},
+    author = {Alec Radford, Ilya Sutskever, Jong Wook Kim, Gretchen Krueger, Sandhini Agarwal},
+    year   = {2021}
+}
+```
+
+```bibtex
+@misc{esser2020taming,
+      title={Taming Transformers for High-Resolution Image Synthesis}, 
+      author={Patrick Esser and Robin Rombach and Björn Ommer},
+      year={2020},
+      eprint={2012.09841},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV}
+}
+```
+
+Katherine Crowson - <https://github.com/crowsonkb>
+NerdyRodent - <https://github.com/nerdyrodent/>
+
+Public Domain images from Open Access Images at the Art Institute of Chicago - <https://www.artic.edu/open-access/open-access-images>
---
+++ setup.py
@@ -0,0 +1,119 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='vqgan-clip-generator',
+    version='0.0.1',
+    description='Implements VQGAN+CLIP and support functions for image and video generation based on text and image prompts',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rkhamilton/vqgan-clip-generator',
+    author='Ryan Hamilton',
+    author_email='ryan.hamilton@gmail.com',
+
+    # Classifiers help users find your project by categorizing it.
+    #
+    # For a list of valid classifiers, see https://pypi.org/classifiers/
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Environment :: GPU :: NVIDIA CUDA',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Image Processing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+
+    keywords='VQGAN, VQGAN+CLIP, deep dream, neural network, pytorch',  # Optional
+
+    # When your source code is in a subdirectory under the project root, e.g.
+    # `src/`, it is necessary to specify the `package_dir` argument.
+    package_dir={'': 'src'},  # Optional
+
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(where='src'),  # Required
+
+    python_requires='>=3.6, <4',
+
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[],  # Optional
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). Users will be able to install these using the "extras"
+    # syntax, for example:
+    #
+    #   $ pip install sampleproject[dev]
+    #
+    # Similar to `install_requires` above, these must be valid existing
+    # projects.
+    extras_require={
+        'test': ['pytest'],
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    package_data={  # Optional
+        'sample': ['package_data.dat'],
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/distutils/setupscript.html#installing-additional-files
+    #
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[('my_data', ['data/data_file'])],  # Optional
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        'console_scripts': [
+            'sample=sample:main',
+        ],
+    },
+
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/rkhamilton/vqgan-clip-generator/issues',
+        'Source': 'https://github.com/rkhamilton/vqgan-clip-generator',
+    },
+)
