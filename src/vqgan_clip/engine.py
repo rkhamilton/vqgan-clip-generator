074970b25a496bbe901eb386beae31ba49b9c22a
Ryan Hamilton <rkhamilton@users.noreply.github.com>
2021-10-07 22:36:10 -0400

    Changed config from a dict to a class

--- src/vqgan_clip/engine.py
+++ src/vqgan_clip/engine.py
@@ -20,46 +20,48 @@ from urllib.request import urlopen
 import numpy as np
 
 
-class Engine:
-    config = {
-        'prompt' : 'A painting of flowers in the renaissance style', # text prompt used to generate an image
-        'image_prompts' : [], # an input image can be parsed by CLIP and used as a prompt to generate another image
-        'iterations' : 100, # number of iterations of train() to perform before stopping.
-        'save_every' : 50, # an interim image will be saved to the output location at an iteration interval defined here
-        'output_image_size' : [256,256], # x/y dimensions of the output image in pixels.
-        'init_image' : None, # a seed image that can be used to start the training. Without an initial image, random noise will be used.
-        'init_noise' : None,
-        'init_weight' : 0.0,
-        'clip_model' : 'ViT-B/32',
-        'vqgan_config' : f'models/vqgan_imagenet_f16_16384.yaml',
-        'vqgan_checkpoint' : f'models/vqgan_imagenet_f16_16384.ckpt',
-        'noise_prompt_seeds' : [],
-        'noise_prompt_weights' : [],
-        'learning_rate' : 0.1,
-        'cut_method' : 'latest',
-        'num_cuts' : 32,
-        'cut_power' : 1.0,
-        'seed' : None,
-        'optimiser' : 'Adam',
-        'output_filename' : 'output.png',
-        'make_video' : False,
-        'make_zoom_video' : False,
-        'zoom_start' : 0,
-        'zoom_save_every' : 50,
-        'zoom_scale' : 1.02,
-        'zoom_shift_x' : 0,
-        'zoom_shift_y' : 0,
-        'change_prompt_every' : 0,
-        'output_video_fps' : 60,
-        'input_video_fps' : 15,
-        'cudnn_determinism' : False,
-        'augments' : [['Af', 'Pe', 'Ji', 'Er']],
-        'video_style_dir' : None,
-        'cuda_device' : 'cuda:0'
-    }
 
+class EngineConfig:
+    def __init__(self):
+        self.prompts = 'A painting of flowers in the renaissance style:0.5|rembrandt:0.5^fish:0.2|love:1'
+        self.image_prompts = []
+        self.iterations = 100 # number of iterations of train() to perform before stopping.
+        self.save_every = 50 # an interim image will be saved to the output location at an iteration interval defined here
+        self.output_image_size = [256,256] # x/y dimensions of the output image in pixels.
+        self.init_image = None # a seed image that can be used to start the training. Without an initial image, random noise will be used.
+        self.init_noise = None
+        self.init_weight = 0.0
+        self.clip_model = 'ViT-B/32'
+        self.vqgan_config = f'models/vqgan_imagenet_f16_16384.yaml'
+        self.vqgan_checkpoint = f'models/vqgan_imagenet_f16_16384.ckpt'
+        self.noise_prompt_seeds = []
+        self.noise_prompt_weights = []
+        self.learning_rate = 0.1
+        self.cut_method = 'latest'
+        self.num_cuts = 32
+        self.cut_power = 1.0
+        self.seed = None
+        self.optimiser = 'Adam'
+        self.output_filename = 'output.png'
+        self.make_video = False
+        self.make_zoom_video = False
+        self.zoom_start = 0
+        self.zoom_save_every = 50
+        self.zoom_scale = 1.02
+        self.zoom_shift_x = 0
+        self.zoom_shift_y = 0
+        self.change_prompt_every = 0
+        self.output_video_fps = 60
+        self.input_video_fps = 15
+        self.cudnn_determinism = False
+        self.augments = [['Af', 'Pe', 'Ji', 'Er']]
+        self.video_style_dir = None
+        self.cuda_device = 'cuda:0'
+
+class Engine:
     def __init__(self):
         # self._optimiser = optim.Adam([self._z], lr=0.1)
+        self.conf = EngineConfig()
         self._iteration_number = 0.0
         self._loss = 0.0
 
@@ -74,8 +76,8 @@ class Engine:
         self.pMs = []
 
         self.image_prompts = []
-        self.all_prompts_phrases = []
-        self.current_prompt_phrase = []
+        self.all_prompts_story_phrases = []
+        self.current_prompt_story_phrase = []
 
     def set_seed(self, seed):
         self.seed = seed
@@ -107,15 +109,15 @@ class Engine:
         #self._optimiser.zero_grad(set_to_none=True)
         lossAll = self.ascend_txt()
         
-        if i % self.config['save_every'] == 0:
+        if i % self.conf.save_every == 0:
             with torch.inference_mode():
                 # TODO move this to outer loop
                 # losses_str = ', '.join(f'{loss.item():g}' for loss in lossAll)
                 # tqdm.write(f'i: {i}, loss: {sum(lossAll).item():g}, lossAll: {losses_str}')
                 out = self.synth()
                 info = PngImagePlugin.PngInfo()
-                info.add_text('comment', self.config['prompt'][0])
-                TF.to_pil_image(out[0].cpu()).save(self.config['output_filename'], pnginfo=info) 	
+                info.add_text('comment', self.current_prompt_story_phrase[0])
+                TF.to_pil_image(out[0].cpu()).save(self.conf.output_filename, pnginfo=info) 	
 
         loss = sum(lossAll)
         loss.backward()
@@ -131,14 +133,14 @@ class Engine:
         
         result = []
 
-        if self.config['init_weight']:
+        if self.conf.init_weight:
             # result.append(F.mse_loss(self._z, z_orig) * args.init_weight / 2)
-            result.append(F.mse_loss(self._z, torch.zeros_like(self._z_orig)) * ((1/torch.tensor(self._iteration_number*2 + 1))*self.config['init_weight']) / 2)
+            result.append(F.mse_loss(self._z, torch.zeros_like(self._z_orig)) * ((1/torch.tensor(self._iteration_number*2 + 1))*self.conf.init_weight) / 2)
 
         for prompt in self.pMs:
             result.append(prompt(iii))
         
-        if self.config['make_video']:    
+        if self.conf.make_video:    
             img = np.array(out.mul(255).clamp(0, 255)[0].cpu().detach().numpy().astype(np.uint8))[:,:,:]
             img = np.transpose(img, (1, 2, 0))
             imageio.imwrite('./steps/' + str(self._iteration_number) + '.png', np.array(img))
@@ -157,20 +159,22 @@ class Engine:
     # main execution path from generate.py
     def do_it(self):
         # Split text prompts using the pipe character (weights are split later)
-        if self.config['prompt']:
+        if self.conf.prompts:
             # For stories, there will be many phrases
-            story_phrases = [phrase.strip() for phrase in self.config['prompt'].split("^")]
+            story_phrases = [phrase.strip() for phrase in self.conf.prompts.split("^")]
             
             # Make a list of all phrases
+            all_prompts_phrases = []
             for phrase in story_phrases:
-                self.all_prompts_phrases.append(phrase.split("|"))
-            
+                all_prompts_phrases.append(phrase.split("|"))
+            self.all_prompts_story_phrases = all_prompts_phrases
+
             # First phrase
-            self.current_prompt_phrase = self.all_prompts_phrases[0]
+            self.current_prompt_story_phrase = self.all_prompts_story_phrases[0]
             
         # Split target images using the pipe character (weights are split later)
-        if self.config['image_prompts']:
-            self.image_prompts = self.config['image_prompts'].split("|")
+        if self.conf.image_prompts:
+            self.image_prompts = self.conf.image_prompts.split("|")
             self.image_prompts = [image.strip() for image in self.image_prompts]
             # Check for GPU and reduce the default image size if low VRAM
 
@@ -181,33 +185,33 @@ class Engine:
             default_image_size = 318  # <8GB VRAM
 
         
-        self._device = torch.device(self.config['cuda_device'])
-        self._model = vm.load_vqgan_model(self.config['vqgan_config'], self.config['vqgan_checkpoint']).to(self._device)
+        self._device = torch.device(self.conf.cuda_device)
+        self._model = vm.load_vqgan_model(self.conf.vqgan_config, self.conf.vqgan_checkpoint).to(self._device)
         jit = True if float(torch.__version__[:3]) < 1.8 else False
-        self._perceptor = clip.load(self.config['clip_model'], jit=jit)[0].eval().requires_grad_(False).to(self._device)
+        self._perceptor = clip.load(self.conf.clip_model, jit=jit)[0].eval().requires_grad_(False).to(self._device)
 
         cut_size = self._perceptor.visual.input_resolution
         f = 2**(self._model.decoder.num_resolutions - 1)
 
         # Cutout class options:
         # 'latest','original','updated' or 'updatedpooling'
-        if self.config['cut_method'] == 'latest':
-            self._make_cutouts = vm.MakeCutouts(cut_size, self.config['num_cuts'], self.config['augments'], cut_pow=self.config['cut_power'])
-        elif self.config['cut_method'] == 'original':
-            self._make_cutouts = vm.MakeCutoutsOrig(cut_size, self.config['num_cuts'], cut_pow=self.config['cut_power'])
-        elif self.config['cut_method'] == 'updated':
-            self._make_cutouts = vm.MakeCutoutsUpdate(cut_size, self.config['num_cuts'], cut_pow=self.config['cut_power'])
-        elif self.config['cut_method'] == 'nrupdated':
-            self._make_cutouts = vm.MakeCutoutsNRUpdate(cut_size, self.config['num_cuts'], self.config['augments'], cut_pow=self.config['cut_power'])
+        if self.conf.cut_method == 'latest':
+            self._make_cutouts = vm.MakeCutouts(cut_size, self.conf.num_cuts, self.conf.augments, cut_pow=self.conf.cut_power)
+        elif self.conf.cut_method == 'original':
+            self._make_cutouts = vm.MakeCutoutsOrig(cut_size, self.conf.num_cuts, cut_pow=self.conf.cut_power)
+        elif self.conf.cut_method == 'updated':
+            self._make_cutouts = vm.MakeCutoutsUpdate(cut_size, self.conf.num_cuts, cut_pow=self.conf.cut_power)
+        elif self.conf.cut_method == 'nrupdated':
+            self._make_cutouts = vm.MakeCutoutsNRUpdate(cut_size, self.conf.num_cuts, self.conf.augments, cut_pow=self.conf.cut_power)
         else:
-            self._make_cutouts = vm.MakeCutoutsPoolingUpdate(cut_size, self.config['num_cuts'], cut_pow=self.config['cut_power'])    
+            self._make_cutouts = vm.MakeCutoutsPoolingUpdate(cut_size, self.conf.num_cuts, cut_pow=self.conf.cut_power)    
 
-        toksX, toksY = self.config['output_image_size'][0] // f, self.config['output_image_size'][1] // f
+        toksX, toksY = self.conf.output_image_size[0] // f, self.conf.output_image_size[1] // f
         sideX, sideY = toksX * f, toksY * f
 
         # CLIP tokenize/encode   
-        if self.config['prompt']:
-            for prompt in self.config['prompt']:
+        if self.current_prompt_story_phrase:
+            for prompt in self.current_prompt_story_phrase:
                 txt, weight, stop = vm.split_prompt(prompt)
                 embed = self._perceptor.encode_text(clip.tokenize(txt).to(self._device)).float()
                 self.pMs.append(vm.Prompt(embed, weight, stop).to(self._device))
@@ -225,23 +229,23 @@ class Engine:
             self.z_max = self._model.quantize.embedding.weight.max(dim=0).values[None, :, None, None]
 
 
-        if self.config['init_image']:
-            if 'http' in self.config['init_image']:
-                output = Image.open(urlopen(self.config['init_image']))
+        if self.conf.init_image:
+            if 'http' in self.conf.init_image:
+                output = Image.open(urlopen(self.conf.init_image))
             else:
-                output = Image.open(self.config['init_image'])
+                output = Image.open(self.conf.init_image)
             pil_image = output.convert('RGB')
             pil_image = pil_image.resize((sideX, sideY), Image.LANCZOS)
             pil_tensor = TF.to_tensor(pil_image)
             self._z, *_ = self._model.encode(pil_tensor.to(self._device).unsqueeze(0) * 2 - 1)
-        elif self.config['init_noise'] == 'pixels':
-            output = vm.random_noise_image(self.config['image_size'][0], self.config['image_size'][1])    
+        elif self.conf.init_noise == 'pixels':
+            output = vm.random_noise_image(self.conf.image_size[0], self.conf.image_size[1])    
             pil_image = output.convert('RGB')
             pil_image = pil_image.resize((sideX, sideY), Image.LANCZOS)
             pil_tensor = TF.to_tensor(pil_image)
             self._z, *_ = self._model.encode(pil_tensor.to(self._device).unsqueeze(0) * 2 - 1)
-        elif self.config['init_noise'] == 'gradient':
-            output = vm.random_gradient_image(self.config['image_size'][0], self.config['image_size'][1])
+        elif self.conf.init_noise == 'gradient':
+            output = vm.random_gradient_image(self.conf.image_size[0], self.conf.image_size[1])
             pil_image = output.convert('RGB')
             pil_image = pil_image.resize((sideX, sideY), Image.LANCZOS)
             pil_tensor = TF.to_tensor(pil_image)
@@ -261,8 +265,8 @@ class Engine:
         self._z.requires_grad_(True)       
         
         # CLIP tokenize/encode   
-        if self.config['prompt']:
-            for prompt in self.config['prompt']:
+        if self.current_prompt_story_phrase:
+            for prompt in self.current_prompt_story_phrase:
                 txt, weight, stop = vm.split_prompt(prompt)
                 embed = self._perceptor.encode_text(clip.tokenize(txt).to(self._device)).float()
                 self.pMs.append(vm.Prompt(embed, weight, stop).to(self._device))
@@ -276,7 +280,7 @@ class Engine:
             embed = self._perceptor.encode_image(vm.normalize(batch)).float()
             self.pMs.append(vm.Prompt(embed, weight, stop).to(self._device))
 
-        for seed, weight in zip(self.config['noise_prompt_seeds'], self.config['noise_prompt_weights']):
+        for seed, weight in zip(self.conf.noise_prompt_seeds, self.conf.noise_prompt_weights):
             gen = torch.Generator().manual_seed(seed)
             embed = torch.empty([1, self._perceptor.visual.output_dim]).normal_(generator=gen)
             self.pMs.append(vm.Prompt(embed, weight).to(self._device))
@@ -287,9 +291,9 @@ class Engine:
         smoother_counter = 0 # Smoother counter
         video_styler_frame_num = 0 # for video styling
 
-        self.set_optimiser(self.config['optimiser'])
+        self.set_optimiser(self.conf.optimiser)
         try:
-            for iteration_num in tqdm(range(1,self.config['iterations']+1)):
+            for iteration_num in tqdm(range(1,self.conf.iterations+1)):
                 self.train(iteration_num)
         except KeyboardInterrupt:
             pass
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,11 +1,8 @@
-import pytest
 from vqgan_clip.engine import Engine
 
 
-
-# generate a single image based on a text prompt
-def test_generate_single_image(prompt_text):
+def single_image(prompt_text):
     eng = Engine()
-    eng.conf.prompts = 'This is a field of flowers'
+    eng.set('prompt',prompt_text)
+    print(eng.config('prompt'))
     eng.do_it()
-    
