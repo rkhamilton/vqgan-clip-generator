fdfad4acf340c980ea6ffcb2c960d7b3a9b7d72e
Ryan Hamilton <rkhamilton@users.noreply.github.com>
2021-10-12 09:24:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,7 @@ import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 
-# Get the long description from the README file
 long_description = (here / 'README.md').read_text(encoding='utf-8')
-
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
-
 setup(
     name='vqgan-clip-generator',
     version='0.1.0',
@@ -18,10 +13,6 @@ setup(
     url='https://github.com/rkhamilton/vqgan-clip-generator',
     author='Ryan Hamilton',
     author_email='ryan.hamilton@gmail.com',
-
-    # Classifiers help users find your project by categorizing it.
-    #
-    # For a list of valid classifiers, see https://pypi.org/classifiers/
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -38,26 +29,10 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
     ],
-
     keywords='VQGAN, VQGAN+CLIP, deep dream, neural network, pytorch',  # Optional
-
-    # When your source code is in a subdirectory under the project root, e.g.
-    # `src/`, it is necessary to specify the `package_dir` argument.
     package_dir={'': 'src'},  # Optional
-
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
     packages=find_packages(where='src'),  # Required
-
     python_requires='>=3.6, <4',
-
     install_requires=[
         'ftfy',
         'regex',
@@ -67,49 +42,11 @@ setup(
         'imageio',
         'omegaconf',
         'taming-transformers',
-        #'imageio-ffmpeg',
-        #'einops',
         'torch_optimizer'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). Users will be able to install these using the "extras"
-    # syntax, for example:
-    #
-    #   $ pip install sampleproject[dev]
-    #
-    # Similar to `install_requires` above, these must be valid existing
-    # projects.
     extras_require={
         'dev' : ['pytest','setuptools'],
         'test': ['pytest'],
     },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.
-    # package_data={  # Optional
-    #     'sample': ['package_data.dat'],
-    # },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/distutils/setupscript.html#installing-additional-files
-    #
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],  # Optional
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
-    # entry_points={  # Optional
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
-
     project_urls={  # Optional
         'Bug Reports': 'https://github.com/rkhamilton/vqgan-clip-generator/issues',
         'Source': 'https://github.com/rkhamilton/vqgan-clip-generator',
