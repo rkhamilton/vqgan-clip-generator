97e9743cd8035782f79b7cd4a1a23ef875c7347c
Ryan Hamilton <rkhamilton@users.noreply.github.com>
2021-10-25 20:44:53 -0400

    tests for each example file

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -4,19 +4,38 @@ import subprocess, os
 THIS_FILE_PATH = os.path.dirname(__file__)
 EXAMPLES_PATH = os.path.join(THIS_FILE_PATH,'..'+os.sep+'examples')
 
-def test_examples():
-    '''Confirm all example scripts run without error
-    '''
-    examples = ['single_image.py',
-                'custom_zoom_video.py',
-                'image_prompt.py',
-                'multiple_same_prompt.py',
-                'restyle_video_naive.py',
-                'restyle_video.py',
-                'single_image.py',
-                'upscaling_video.py',
-                'video.py',
-                'zoom_video.py']    
-    for example in examples:
-        cmnd = 'python ' + os.path.join(EXAMPLES_PATH,example)
-        subprocess.check_output(cmnd)
+def test_examples_single_image():
+    cmnd = 'python ' + os.path.join(EXAMPLES_PATH,'single_image.py')
+    subprocess.check_output(cmnd)
+
+def test_examples_custom_zoom_video():
+    cmnd = 'python ' + os.path.join(EXAMPLES_PATH,'custom_zoom_video.py')
+    subprocess.check_output(cmnd)
+
+def test_examples_image_prompt():
+    cmnd = 'python ' + os.path.join(EXAMPLES_PATH,'image_prompt.py')
+    subprocess.check_output(cmnd)
+
+def test_examples_multiple_same_prompt():
+    cmnd = 'python ' + os.path.join(EXAMPLES_PATH,'multiple_same_prompt.py')
+    subprocess.check_output(cmnd)
+
+def test_examples_restyle_video_naive():
+    cmnd = 'python ' + os.path.join(EXAMPLES_PATH,'restyle_video_naive.py')
+    subprocess.check_output(cmnd)
+
+def test_examples_restyle_video():
+    cmnd = 'python ' + os.path.join(EXAMPLES_PATH,'restyle_video.py')
+    subprocess.check_output(cmnd)
+
+def test_examples_upscaling_video():
+    cmnd = 'python ' + os.path.join(EXAMPLES_PATH,'upscaling_video.py')
+    subprocess.check_output(cmnd)
+
+def test_examples_video():
+    cmnd = 'python ' + os.path.join(EXAMPLES_PATH,'video.py')
+    subprocess.check_output(cmnd)
+
+def test_examples_zoom_video():
+    cmnd = 'python ' + os.path.join(EXAMPLES_PATH,'zoom_video.py')
+    subprocess.check_output(cmnd)
