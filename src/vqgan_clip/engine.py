6c2900b487b2473c1bf2383bf8ea6dfd553d9549
Ryan Hamilton <rkhamilton@users.noreply.github.com>
2021-10-27 15:51:17 -0400

    bug in init_image_method error handling

--- src/vqgan_clip/engine.py
+++ src/vqgan_clip/engine.py
@@ -208,7 +208,7 @@ class Engine:
             elif self.conf.init_image_method == 'alternate_img_target_decay':
                 result.append(F.mse_loss(self._z, self._alternate_img_target) * ((self.conf.init_weight * 5/torch.tensor(iteration_number*2 + 1))))
             else:
-                raise NameError('Invalid init_weight_method f{self.conf.init_image_method}')
+                raise NameError(f'Invalid init_weight_method {self.conf.init_image_method}')
 
         for prompt in self.pMs:
             result.append(prompt(encoded_image))
