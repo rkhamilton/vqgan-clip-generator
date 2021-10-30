d24fee58beb86f4bf3b3727d29febf2a5c3193be
Ryan Hamilton <rkhamilton@users.noreply.github.com>
2021-10-29 22:01:35 -0400

    still more linux fixes

--- src/vqgan_clip/video_tools.py
+++ src/vqgan_clip/video_tools.py
@@ -16,7 +16,7 @@ def extract_video_frames(input_video_path, extraction_framerate, extracted_video
         List of str: List of paths to result frames, sorted by filename.
     """
     # Parse original video file into individual frames
-    # original_video = 'video_restyle\\original_video\\20211004_132008000_iOS.MOV'
+    # original_video = 'video_restyle{os.sep}original_video{os.sep}20211004_132008000_iOS.MOV'
     # extraction_framerate = '30' # number of frames per second to extract from the original video
 
     # clean up arguments for use on shell if needed
@@ -134,7 +134,7 @@ def RIFE_interpolation(input, output, interpolation_factor=4, metadata_title='',
     # we need the framerate of the source video to know what name RIFE will use in the output
     input_framerate = cv2.VideoCapture(input).get(cv2.CAP_PROP_FPS)
 
-    of_cmnd = f'python arXiv2020-RIFE\\inference_video.py --exp={2 if interpolation_factor==4 else 4} --model=arXiv2020-RIFE\\train_log --video={enquote_paths_with_spaces(input)}'
+    of_cmnd = f'python arXiv2020-RIFE{os.sep}inference_video.py --exp={2 if interpolation_factor==4 else 4} --model=arXiv2020-RIFE{os.sep}rain_log --video={enquote_paths_with_spaces(input)}'
     subprocess.Popen(of_cmnd, shell=True).wait()
     # print(f'RIFE optical flow command used was:\n{of_cmnd}')
 
